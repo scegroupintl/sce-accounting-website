34a59080ac7440e4bdc62f7c7b4870466a0ed65a
hperezs <h.perez.saldivar@gmail.com>
2025-09-12 11:29:12 -0600

    Update footer company name

--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-gradient-to-b from-teal-700 via-teal-800 to-teal-900 text-white py-6">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
         <div className="mb-2 md:mb-0 text-center text-sm md:text-base md:text-left">
-          &copy; {new Date().getFullYear()} SCE Accounting Services. Todos los
+          &copy; {new Date().getFullYear()} SCE Accounting LLC. Todos los
           derechos reservados.
         </div>
         <div className="flex flex-col md:flex-row items-center gap-2 text-xs md:text-sm">
